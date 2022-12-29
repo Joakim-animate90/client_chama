54a685d8430cac0d6279cdce7881c6e5de95f9e3
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-12-29 16:07:18 +0300

    Remove deprecated versions on some properties of ThemeData class

--- lib/src/themes/global_themes.dart
+++ lib/src/themes/global_themes.dart
@@ -4,19 +4,25 @@ import 'package:flutter/material.dart';
 class GlobalTheme {
   static ThemeData lightTheme = ThemeData(
     primaryColor: kSecondaryColor,
-    accentColor: kBlack,
     scaffoldBackgroundColor: kSecondaryColor,
-    appBarTheme: const AppBarTheme(
+    appBarTheme: AppBarTheme(
       color: kPrimaryColor,
       elevation: 0,
-      iconTheme: IconThemeData(color: kBlack),
-      textTheme: TextTheme(
+      iconTheme: const IconThemeData(color: kBlack),
+      toolbarTextStyle: const TextTheme(
         headline6: TextStyle(
           color: kBlack,
           fontSize: 20,
           fontWeight: FontWeight.w600,
         ),
-      ),
+      ).bodyText2,
+      titleTextStyle: const TextTheme(
+        headline6: TextStyle(
+          color: kBlack,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ).headline6,
     ),
     textTheme: const TextTheme(
       headline6: TextStyle(
@@ -30,23 +36,30 @@ class GlobalTheme {
         fontWeight: FontWeight.w400,
       ),
     ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kBlack),
   );
 
   static ThemeData darkTheme = ThemeData(
     primaryColor: kBlack,
-    accentColor: kSecondaryColor,
     scaffoldBackgroundColor: kBlack,
-    appBarTheme: const AppBarTheme(
+    appBarTheme: AppBarTheme(
       color: kBlack,
       elevation: 0,
-      iconTheme: IconThemeData(color: kSecondaryColor),
-      textTheme: TextTheme(
+      iconTheme: const IconThemeData(color: kSecondaryColor),
+      toolbarTextStyle: const TextTheme(
         headline6: TextStyle(
           color: kSecondaryColor,
           fontSize: 20,
           fontWeight: FontWeight.w600,
         ),
-      ),
+      ).bodyText2,
+      titleTextStyle: const TextTheme(
+        headline6: TextStyle(
+          color: kSecondaryColor,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ).headline6,
     ),
     textTheme: const TextTheme(
       headline6: TextStyle(
@@ -60,5 +73,6 @@ class GlobalTheme {
         fontWeight: FontWeight.w400,
       ),
     ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kSecondaryColor),
   );
 }
